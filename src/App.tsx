e71e741f77be86bb9f014f6c5e1e504cd8fb0c1a
Geniecko <radoslawkania09@gmail.com>
2022-08-18 21:55:28 +0200

    create actions and reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import MainTemplate from './templates/MainTemplate'
+
+import { getWeather } from './store/actions/weatherActions';
+import { useAppDispatch } from './store/hooks';
+import MainTemplate from './templates/MainTemplate';
 
 const App = () => {
-  return <MainTemplate></MainTemplate>
-}
+  const dispatch = useAppDispatch();
 
-export default App
+  const getData = async () => {
+    dispatch(await getWeather('warsaw'));
+  }
+  
+  return (
+    <>
+      <MainTemplate>
+        <h1>asd</h1>
+      </MainTemplate>
+      <button onClick={getData}>Pobierz dane</button>
+    </>
+  );
+};
+
+export default App;
---
+++ src/store/actions/weatherActions.ts
@@ -0,0 +1,44 @@
+import { API_KEY, API_URL } from '../../api/data';
+import {
+  WeatherAction,
+  SET_LOADING,
+  WeatherData,
+  SET_ERROR,
+  GET_WEATHER,
+} from '../types';
+
+export const getWeather = async (city: string) => {
+  try {
+    const response = await fetch(`${API_URL}&q=${city}&appid=${API_KEY}`);
+
+    if (!response.ok) {
+      return {
+        type: SET_ERROR,
+        error: {
+          message: 'Error !respone.ok',
+        },
+      };
+
+      console.log(response);
+    }
+
+    const responseData: WeatherData = await response.json();
+
+    return {
+      type: GET_WEATHER,
+      payload: responseData,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      type: SET_ERROR,
+      payload: 'Somethink wents wrong',
+    };
+  }
+};
+
+export const setLoading = (): WeatherAction => {
+  return {
+    type: SET_LOADING,
+  };
+};
