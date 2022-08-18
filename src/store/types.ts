3110b6a20b2585fc00d34ab4fe45b862b1e6eb27
Geniecko <radoslawkania09@gmail.com>
2022-08-18 21:54:36 +0200

    instal redux, configure store and create types file

---
+++ src/store/types.ts
@@ -0,0 +1,73 @@
+export const GET_WEATHER = 'GET_WEATHER';
+export const SET_LOADING = 'GET_LOADING';
+export const SET_ERROR = 'GET_ERROR';
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface WeatherData {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: Weather[];
+  wind: {
+    speed: number;
+    deg: number;
+  };
+}
+
+export interface WeatherError {
+  message: string;
+}
+
+export interface WeatherState {
+  data: WeatherData | null | undefined;
+  loading: boolean;
+  error: string;
+}
+
+interface GetWeatherAction {
+  type: typeof GET_WEATHER;
+  payload?: WeatherData;
+}
+
+interface SetLoadingAction {
+  type: typeof SET_LOADING;
+}
+
+interface SetErrorAction {
+  type: typeof SET_ERROR;
+  payload: string;
+}
+
+export type WeatherAction = GetWeatherAction | SetLoadingAction | SetErrorAction;
