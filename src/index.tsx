1b345e8597209816e174fcb1e5f72a684d2661f7
Geniecko <radoslawkania09@gmail.com>
2022-08-20 22:12:21 +0200

    add browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 );
