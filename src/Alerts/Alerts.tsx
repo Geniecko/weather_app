2852339f1ca837fd1284c694e616c55f5188a538
Geniecko <radoslawkania09@gmail.com>
2022-08-20 17:13:06 +0200

    refactor, remove mainview component and move all into App.tsx

---
+++ src/Alerts/Alerts.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import Loading from '../components/Loading/Loading';
+import { useAppSelector } from '../store/hooks';
+
+
+const Alerts: FC = () => {
+  const isLoading = useAppSelector(state => state.weather.loading);
+  const error = useAppSelector(state => state.weather.error);
+
+  return ( 
+    <StyledAlerts>
+      <Alert>
+          {isLoading && <Loading />}
+        </Alert>
+        <Alert>
+          {error.message > '' ? 'No results found' : null}
+        </Alert>
+    </StyledAlerts>
+   );
+}
+ 
+const StyledAlerts = styled.div`
+  padding: 16px 0;
+  position: relative;
+  height: 25px;
+`;
+
+const Alert = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default Alerts;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import MainTemplate from './templates/MainTemplate';
-import MainView from './views/MainView';
+import styled from 'styled-components/macro';
+import Alerts from './Alerts/Alerts';
+import SearchBar from './components/SearchBar/SearchBar';
+import CurrentWeather from './components/CurrentWeather/CurrentWeather';
+import { useAppSelector } from './store/hooks';
 
 const App = () => {
+  const weather = useAppSelector(state => state.weather.data);
+
   return (
     <MainTemplate>
       <div className='app-overlay'>
-        <MainView />
+        <Container>
+          <SearchBar />
+          <Alerts />
+          {weather && <CurrentWeather weather={weather} />}
+        </Container>
       </div>
     </MainTemplate>
   );
 };
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default App;
