b6a55b9f53cc590190eee44c47185f7a19b89993
Geniecko <radoslawkania09@gmail.com>
2022-08-25 20:56:28 +0200

    change air polution and foreact url on https

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,3 +1,3 @@
 export const API_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-export const API_AIR_POLUTION_URL = 'http://api.openweathermap.org/data/2.5/air_pollution?';
-export const API_FORECAST_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
+export const API_AIR_POLUTION_URL = 'https://api.openweathermap.org/data/2.5/air_pollution?';
+export const API_FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?';
